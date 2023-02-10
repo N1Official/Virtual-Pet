e4a7bf7d22e915d04fac0152aeec7d996760bf70
László Dao <56759208+N1Official@users.noreply.github.com>
2023-02-10 21:01:44 +0100

    Osztalyok elkeszitese

---
+++ Administration/Administration/Pet.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Administration
+{
+    internal class Pet
+    {
+        public List<string> Types = new List<string>
+        {
+            "tiger",
+            "panther",
+            "lion",
+            "snake",
+            "dog",
+            "cat",
+            "hamster"
+        };
+        public List<string> Moods = new List<string>
+        {
+            "Good",
+            "Tired",
+            "Mad",
+            "Thankful",
+            "Touched",
+            "Lonely",
+            "loved",
+            "Stressed",
+            "Bored",
+        };
+        //These are temporarly while the backend is on developing
+        private string name;
+        private string type;
+        private int hunger;
+        private int thirst;
+        private string mood;
+        private int speed;
+        private int health;
+        private int skill;
+        private int age;
+        private string age_group;
+
+        public string Name { get => name; set => name = value; }
+        public int Hunger 
+        { 
+            get => hunger;
+            set 
+            {
+                if (value<0)
+                {
+                    MessageBox.Show("Negatív nem lehet az éhségszint!");
+                }
+                else
+                {
+                    hunger = value;
+                }
+            } 
+        }
+        public int Thirst 
+        {
+            get => thirst;
+            set
+            {
+                if (value < 0)
+                {
+                    MessageBox.Show("Negatív nem lehet a szomjúság!");
+                }
+                else
+                {
+                    thirst = value;
+                }
+            }
+        }
+        public string Mood 
+        { 
+            get => mood;
+            set
+            {
+                if (Moods.Contains(value.ToLower()))
+                {
+                    mood = value;
+                }
+                else
+                {
+                    MessageBox.Show("A megadott kedv még nincs hozzáadva!");
+                }
+            }
+        }
+        public int Speed
+        {
+            get => speed;
+            set
+            {
+                if (value <= 0)
+                {
+                    MessageBox.Show("Nulla, illetve annál kisebb nem lehet a gyorsaság!");
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+        public int Health 
+        {
+            get => health;
+            set
+            {
+                if (value <= 0)
+                {
+                    MessageBox.Show("Nulla, illetve annál kisebb nem lehet az egészség!");
+                }
+                else
+                {
+                    health = value;
+                }
+            }
+        }
+        public int Skill 
+        { 
+            get => skill;
+            set
+            {
+                if (value < 0)
+                {
+                    MessageBox.Show("Negatív nem lehet az ügyeszség!");
+                }
+                else
+                {
+                    skill = value;
+                }
+            }
+        }
+        public int Age 
+        {
+            get => age;
+            set
+            {
+                if (value < 0)
+                {
+                    MessageBox.Show("Negatív nem lehet a kor!");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        public string Age_group 
+        {
+            get => age_group;
+            set 
+            {
+                if (age<100 || value=="Kölyök")
+                {
+                    value = "Kölyök";
+                    age_group = value;
+                }
+                if ((age>=100 && age<200) || value=="Felnőtt")
+                {
+                    value = "Felnőtt";
+                    age_group = value;
+                }
+                if ((age >= 200 && age < 300) || value == "Öreg")
+                {
+                    value = "Öreg";
+                    age_group = value;
+                }
+                if (age >299 || value=="Legendary")
+                {
+                    value = "Legendary";
+                    age_group = value;
+                }
+                else
+                {
+                    MessageBox.Show("Hiba a korcsoport megadásakor!");
+                }
+            } 
+        }
+        public string Type 
+        { 
+            get => type;
+            set 
+            {
+                if (Types.Contains(value.ToLower()))
+                {
+                    type = value;
+                }
+                else
+                {
+                    MessageBox.Show("A megadott típus még nem létezik!");
+                }
+            } 
+        }
+    }
+}
---
+++ Administration/Administration/User.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Administration
+{
+    internal class User
+    {
+        private string id;
+        private string name;
+        private string email;
+        private string password;
+        private string firstName;
+        private string lastName;
+        private string gender;
+        private string birthday;
+        private string phone;
+        private string address;
+        private string city;
+        private string state;
+        private int zipcode;
+        private string country;
+
+        public string Id { get => id;}
+        public string Name { get => name; set => name = value; }
+        public string Email
+        { 
+            get => email;
+            set{
+                static bool IsValidEmail(string mail)
+                {
+                    try
+                    {
+                        var addr = new System.Net.Mail.MailAddress(mail);
+                        return addr.Address == mail;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+                bool isOk = IsValidEmail(value);
+                if (isOk)
+                {
+                    email = value;
+                }
+                else
+                {
+                    MessageBox.Show("Hibás email cím!");
+                }
+            } 
+        }
+        public string Password { get => password; set => password = value; }
+        public string FirstName 
+        { 
+            get => firstName;
+            set {
+                bool result;
+                result = Char.IsUpper(value, 0);
+                if (result)
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    string big = (value.ToUpper());
+                    firstName = big.Substring(0, 1) + value.Substring(1);
+                }
+            } 
+        }
+        public string LastName
+        { 
+            get => lastName;
+            set
+            {
+                bool result;
+                result = Char.IsUpper(value, 0);
+                if (result)
+                {
+                    lastName = value;
+                }
+                else
+                {
+                    string big = (value.ToUpper());
+                    lastName = big.Substring(0, 1) + value.Substring(1);
+                }
+            }
+        }
+        public string Gender 
+        { 
+            get => gender;
+            set 
+            {
+                if (value == "ferfi" || value == "férfi" || value == "male" || value == "Male" || value == "Férfi")
+                {
+                    gender = value;
+                }
+                else if (value == "no" || value == "nő" || value == "Female" || value == "female" || value == "Nő")
+                {
+                    gender = value;
+                }
+                else
+                {
+                    MessageBox.Show("Hibás nem!");
+                }
+            }
+        }
+        public string Birthday { get => birthday; set => birthday = value; }
+        public string Phone 
+        { 
+            get => phone;
+            set
+            {
+                if (value.StartsWith("+"))
+                {
+                    phone = value;
+                }
+                else
+                {
+                    MessageBox.Show("Kérem országos hívószámmal adja meg a telefonszámot!");
+                }
+            }
+        }
+        public string Address { get => address; set => address = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public int Zipcode 
+        { 
+            get => zipcode;
+            set
+            {
+                try
+                {
+                    zipcode = value;
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show("Valami hiba történt: {0}", ex.Message);
+                }
+
+            }
+        }
+        public string Country { get => country; set => country = value; }
+    }
+}

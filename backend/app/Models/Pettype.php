7ef8fda1b3d1b2ad08904b431235f5d3e94b317b
Mlevi0 <55492580+Mlevi0@users.noreply.github.com>
2023-02-23 11:00:28 +0100

    mood,pettype, user model

---
+++ backend/app/Models/Pettype.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pettype extends Model
+{
+    use HasFactory;
+    protected $table = 'pettype';
+    protected $primaryKey = 'id';
+}

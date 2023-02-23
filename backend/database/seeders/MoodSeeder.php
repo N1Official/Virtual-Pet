f5442189cc19605a608b11a739b6e027bd82634d
Mlevi0 <55492580+Mlevi0@users.noreply.github.com>
2023-02-23 11:12:42 +0100

    moodseeder data

--- backend/database/seeders/MoodSeeder.php
+++ backend/database/seeders/MoodSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MoodSeeder extends Seeder
 {
@@ -14,6 +15,40 @@ class MoodSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table("moods")->insert([
+            'mood'=> 'Good',
+        ]);
+      
+        DB::table("moods")->insert([
+            'mood'=> 'Tired',
+        ]);
+        
+        DB::table("moods")->insert([
+            'mood'=> 'Mad',
+        ]);
+       
+        DB::table("moods")->insert([
+            'mood'=> 'Thankful',
+        ]);
+
+        DB::table("moods")->insert([
+            'mood'=> 'Touched',
+        ]);
+
+        DB::table("moods")->insert([
+            'mood'=> 'Lonely',
+        ]);
+
+        DB::table("moods")->insert([
+            'mood'=> 'Loved',
+        ]);
+
+        DB::table("moods")->insert([
+            'mood'=> 'Stressed',
+        ]);
+
+        DB::table("moods")->insert([
+            'mood'=> 'Bored',
+        ]);
     }
 }
